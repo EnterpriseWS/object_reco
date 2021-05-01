08f46ee38cfde7b5af1bbbedb3829c3e78f1851f
Robert Wang <robert.wang@enterprisews.com>
2021-04-30 23:24:07 -0700

    Added more error handling code

--- video_capture.py
+++ video_capture.py
@@ -19,23 +19,33 @@ class VideoCapture:
         self._stop_capture = False
 
     def capture(self, fp=None, is_continuous=False, video_span=0, image_interval=0, max_size=0) -> None:
-        ret, frame = self._vc.read()
-        current_image = plt.imshow(frame, cmap='gray')
-        plt.ion()
+        try:
+            ret, frame = self._vc.read()
+            # Reserved for gray images.
+            # current_image = plt.imshow(frame, cmap='gray')
+            # The initial frame is required for the following frames.
+            current_image = plt.imshow(frame)
+            plt.ion()
 
-        while not self._stop_capture:
-            # During the read() action, it blocks the thread.
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                self._stop_capture = True
-                break
-            ret, next_frame = self._vc.read()
-            cvt_image = cv2.cvtColor(next_frame, cv2.COLOR_BGR2GRAY)
-            current_image.set_data(cvt_image)
-            # CAUTION: pause() action is required for displaying correctly,
-            #          it yields to the GUI event loop.
-            plt.pause(float(1/FRAME_RATE))
+            while not self._stop_capture:
+                # # During the read() action, it blocks the thread.
+                # if cv2.waitKey(1) & 0xFF == ord('q'):
+                #     self._stop_capture = True
+                #     break
+                start_time = time.time()
+                ret, next_frame = self._vc.read()
+                # Reserved for gray images.
+                # cvt_image = cv2.cvtColor(next_frame, cv2.COLOR_BGR2GRAY)
+                cvt_image = cv2.cvtColor(next_frame, cv2.COLOR_BGR2RGB)
+                current_image.set_data(cvt_image)
+                print(f'frame_time: {time.time() - start_time}')
+                # CAUTION: pause() action is required for displaying correctly
+                #          because it yields to the GUI event loop.
+                plt.pause(float(1/FRAME_RATE))
 
-        plt.ioff()
-        plt.show()
+            # NOTE: Both ioff() and show() methods are not needed for displaying
+            #       once the set_data() is used.
+        except Exception as ex:
+            print(f'*** Something wrong: {ex}')
         self._vc.release()
         cv2.destroyAllWindows()
